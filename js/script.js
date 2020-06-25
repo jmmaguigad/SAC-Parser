a6afd04863b47afb47c9846f816528259aada7fd
JM Maguigad <jmmaguigad@users.noreply.github.com>
2020-06-26 03:14:31 +0800

    Update script.js
    Updated code by adding validation and alert notification for compiling process

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $('#validate').on("click",function(){
+    // validation
+    var fileforupload = $('#file')[0].files[0];
+
+    if ($('#psgc').val() == "" || typeof fileforupload === "undefined"){
+        alert("Please check municipality/city psg code and file to be sanitized.");
+    }
+
     var form = $('form')[0];
     var formData = new FormData();
     formData.append('forsanitizefile', $('input[type=file]')[0].files[0]);
@@ -9,8 +16,10 @@ $('#validate').on("click",function(){
         contentType:false,
         data: formData,
         success: function (response) {
+            alert("Please wait while compiling next process.");
+            top.location.href = "lib/dupdownloader.php";
             $('#result').html(response);
-        },
+         },
         error: function(data){
             alert("Uh oh! Please close the csv file before checking!");
         }
